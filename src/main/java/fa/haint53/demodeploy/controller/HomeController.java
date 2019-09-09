d11ac029c376a42e956edfba968e8b775675215e
unknown <nthai.uneti@gmail.com>
2019-09-09 15:18:38 +0700

    asda

---
+++ src/main/java/fa/haint53/demodeploy/controller/HomeController.java
@@ -0,0 +1,14 @@
+package fa.haint53.demodeploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping(value = {"/","home"})
+    public String getHomepage(){
+        return "this is homepage";
+    }
+}
